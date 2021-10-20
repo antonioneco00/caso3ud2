61549d889c1218138d2d67726b08ca51e86cacca
antonioneco00 <antonionc2000@hotmail.es>
2021-10-20 13:42:30 +0200

    Añadir

---
+++ adios.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Segundo php</title>
+  </head>
+  <body>
+      <?='<h1>Hasta luego desarrollador!!!</h1>'?>
+  </body>
+</html>
---
+++ hola.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Primer php</title>
+  </head>
+  <body>
+    <?php echo "Hola Paco campeon!!!! <br>" ?>
+    <a href="adios.php">Clic para salir</a>
+  </body>
+</html>
